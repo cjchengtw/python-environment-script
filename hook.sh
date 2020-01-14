21898585199c850a26a852570a5623188e685a42
HY <jonathan950400@gmail.com>
2020-01-14 18:15:08 +0800

    Update hook.sh

--- hook.sh
+++ hook.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 wget https://raw.githubusercontent.com/cjchengtw/python-environment-script/master/pipenv_install.sh
 wget https://raw.githubusercontent.com/cjchengtw/python-environment-script/master/pyenv_install.sh
-PYENV_INSTALL="./pyenv-install.sh" 
-PIPENV_INSTALL="./pipenv-install.sh"
+wget https://raw.githubusercontent.com/cjchengtw/python-environment-script/master/docker_install.sh
+PYENV_INSTALL="./pyenv_install.sh" 
+PIPENV_INSTALL="./pipenv_install.sh"
+DOCKER_INSTALL"./docker_install.sh"
 bash "$PYENV_INSTALL"
 bash "$PIPENV_INSTALL"
+bash "$DOCKER_INSTALL"
