a3468d728fe5739c0730cbceb39da1bc0a9dd51e
HY <jonathan950400@gmail.com>
2020-01-14 15:55:16 +0800

    Create pyenv_install.sh

---
+++ pyenv_install.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+# exit if any error occurs
+set -e
+
+# Ubuntu 18.04 system update and install related dependices 
+echo "========== System Update.... =========="
+echo $SYS_PASSWORD | sudo -S apt update  -y
+
+echo "====== Update Again ======"
+echo $SYS_PASSWORD | sudo -S apt update -y
+echo "Update Finish"
+
+echo "========== Install Dependencies ========="
+echo $SYS_PASSWORD | sudo -S apt install make gcc git vim curl build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev make gcc  zlib1g-dev -y
+echo "=== Installed ==="
+
+
+# install pyenv
+echo "========== Install pyenv using curl =========="
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash 
+echo "========== Installed =========="
+
+# set environment variables
+echo "========== Setting Environment Variables =========="
+ex -sc '$a
+export PATH="/$HOME/.pyenv/bin:$HOME/.local/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PIPENV_VENV_IN_PROJECT=1
+.
+$-,$!uniq
+x' ~/.bashrc
+
+echo "========== Finish Setting =========="
+
+# activate the shell config
+echo "Activate shell"
+.$SHELL
+echo "Activated"
